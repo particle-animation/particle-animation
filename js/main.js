db160036b708919a02a77d2f60d4da0f25a447ea
Klaudio Milankovic <klaudio.milankovic@gmail.com>
2019-05-02 12:17:44 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,74 +10,214 @@ var canvasSizes = {
 // w and h are Vars corresponding to the Window Size
 w = ctx.canvas.width = canvasSizes.width;
 h = ctx.canvas.height = canvasSizes.height;
+/*
+c9e6f6
+8cceea
+cde1f5
+8dceea
+daedf9
+e2f3f9
+ddebf8
+*/
+var particlesData =  [
+    {
+        'x' : 50,
+        'y' : 50,
+        'r' : 6,
+        'color': '#8dceea'
+    },
+    {
+        'x': 40,
+        'y': h/2,
+        'r': 8,
+        'color': '#8ccfea'
+    },
+    {
+        'x': 65,
+        'y': h/2-50,
+        'r': 6,
+        'color': '#cde1f5'
+    },
+    {
+        'x': 15,
+        'y': h/2-20,
+        'r': 7,
+        'color': '#e2f3f9'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
+    {
+        'x': 10,
+        'y': 10,
+        'r': 9,
+        'color': '#cae8f7'
+    },
 
-var positions =  [
-    {
-        'x' : 10,
-        'y' : 10,
-    },
-    {
-        'x' : 10,
-        'y' : h/2,
-    },
-    {
-        'x' : 10,
-        'y' : h/3,
-    },
-    {
-        'x' : 10,
-        'y' : h - 10,
-    },
-    {
-        'x': w - 10,
-        'y': h/7,
-    },
-    {
-        'x': w - 10,
-        'y': h / 4,
-    },
-    {
-        'x': w - 10,
-        'y': h / 2,
-    },
-    {
-        'x': w /2,
-        'y': 10,
-    },
-    {
-        'x': w / 4,
-        'y': 10,
-    },
-    {
-        'x': w * 0.75,
-        'y': 10,
-    },
-    {
-        'x': w * 0.75,
-        'y': h - 10,
-    },
-    {
-        'x': w / 2,
-        'y': h - 10,
-    },
+
 ]
 
 
 // Variables
 particles = [];
 maxParticles = 30;
-radius = 10;
 
 // Once this function is called it will push Objects with multiple Properties into the particles Array
 function createParticles() {
     // Push Particle Objects into the array
     for (var i = 0; i < maxParticles; i++) {
-        var randomPosition = Math.floor(Math.random() * positions.length);
         particles.push({
-            x: positions[randomPosition].x, //Random Position between 0 and w(canvas-width)
-            y: positions[randomPosition].y, // Same here for height
-            xv: Math.random() * 2 - 1, // X-Velocity
-            yv: Math.random() * 2 - 1 // Y-Velocity
+            x: particlesData[i].x,
+            y: particlesData[i].y,
+            xv: 0,//Math.random() * 2 - 1, // X-Velocity
+            yv: 0//Math.random() * 2 - 1 // Y-Velocity
         });
     }
 }
@@ -90,10 +230,11 @@ function draw() {
     // Loop each particle
     for (var i = 0; i < particles.length; i++) {
         ctx.beginPath();
-        // Use the X-Position to Color the Particle
-        ctx.fillStyle = "cae8f7";
+
+        ctx.fillStyle = particlesData[i].color;
+
         // arc parameters: X-Pos, Y,Pos, Radius, Angle(2*PI = 360°)
-        ctx.arc(particles[i].x, particles[i].y, radius, 0, Math.PI * 2);
+        ctx.arc(particles[i].x, particles[i].y, particlesData[i].r, 0, Math.PI * 2);
         ctx.fill();
         ctx.closePath();
     }
@@ -145,7 +286,7 @@ function collision() {
 
         // Detect Collision with the Canvas Borders
         // IF particle X Position is smaller then 0 (left border) OR bigger then Window-Width invert the Velocity
-        if (particles[i].x < 0 + radius || particles[i].x + radius > w) {
+        if (particles[i].x < 0 + particlesData[i].r || particles[i].x + particlesData[i].r > w) {
             particles[i].xv = -particles[i].xv;
             // Because we invert only the Velocity of the Axis the Particle hits, we dont need to calculate the new direction
         }
@@ -155,7 +296,7 @@ function collision() {
             particles[i].yv = -particles[i].yv;
         }
         // Same for the Y-Axis
-        if (particles[i].y - radius < 0 || particles[i].y + radius > h) {
+        if (particles[i].y - particlesData[i].r < 0 || particles[i].y + particlesData[i].r > h) {
             particles[i].yv = -particles[i].yv;
         }
     }
